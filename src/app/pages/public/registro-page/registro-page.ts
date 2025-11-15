c05f581648518bf649e6b88b86d0917c7a7f5292
Donato Laynes <donato.laynes@outlook.es>
2025-11-14 21:04:53 -0500

    Registro de usuarios (2)

--- src/app/pages/public/registro-page/registro-page.ts
+++ src/app/pages/public/registro-page/registro-page.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
+
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { TIPOS_DOCUMENTO } from '../../../domain/tipo-documento';
+import { GENEROS } from '../../../domain/genero';
+import { ESTADOS_CIVILES } from '../../../domain/estado-civil';
+import { mustMatch } from '../../../validators/must-match';
+import { AuthService } from '../../../services/auth-service';
+import { Usuario } from '../../../domain/usuario';
 
 @Component({
   selector: 'app-registro-page',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './registro-page.html',
-  styleUrl: './registro-page.scss'
+  styleUrl: './registro-page.scss',
+  standalone: true,
 })
-export class RegistroPage {
+export class RegistroPage implements OnInit {
+  registerForm!: FormGroup;
 
+  TIPOS_DOCUMENTO = TIPOS_DOCUMENTO;
+
+  GENEROS = GENEROS;
+
+  ESTADOS_CIVILES = ESTADOS_CIVILES;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      nombres: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      passwordConfirm: ['', Validators.required],
+      tipoDocumento: ['', Validators.required],
+      numeroDocumento: ['', Validators.required],
+      estadoCivil: ['', Validators.required],
+      genero: ['', Validators.required],
+      telefono: ['', Validators.required],
+    }, { validators: mustMatch('password', 'passwordConfirm') });
+  }
+
+  onSubmit() {
+    this.authService.register(this.registerForm.value as Usuario).subscribe(res=>{
+      if(res){
+        alert("Su cuenta ha sido creada satisfactoriamente!");
+        this.router.navigateByUrl('/login');
+      }
+    });
+
+  }
 }
